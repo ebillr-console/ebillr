48a6d7fa49c0e6d2eca48b79bd06985ca375e12c
ebillr-console <ebillr.console@gmail.com>
2025-08-25 23:07:27 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return <div className="flex flex-col items-center justify-center h-screen">
+    <p className="text-2xl font-semibold">
+      Home page
+    </p>
+  </div>;
+}
